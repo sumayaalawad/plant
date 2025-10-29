4623c71f346b4f20acdf4da9ab51708349d26b0d
sumayaalawad <sumaiah-119@gotmail.com>
2025-10-29 10:19:06 +0300

    Initial Commit

---
+++ plants/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  plants
+//
+//  Created by sumaya alawad on 07/05/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ plants/plantsApp.swift
@@ -0,0 +1,17 @@
+//
+//  plantsApp.swift
+//  plants
+//
+//  Created by sumaya alawad on 07/05/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct plantsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
